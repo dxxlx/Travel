a056a3216bc0e04a69d634237b22dbea2385a393
lx <690305622@qq.com>
2018-05-22 16:11:14 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wraper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+
+</template>
+<script>
+  export default {
+    name:'HomeSwiper',
+    data(){
+      return{
+        swiperOption:{
+          pagination:'.swiper-pagination',
+          loop:true
+        },
+        swiperList:[
+          {
+            id:'001',
+            imgUrl:'https://img1.qunarzz.com/vc/6a/36/df/4415fc5ed7c08d33649807f321.jpg'
+          },
+          {
+            id:'002',
+            imgUrl:'https://img1.qunarzz.com/vc/c1/c3/4f/7cd9da5aadf1c5b15cdbe11424.jpg'
+          }
+        ]
+      }
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+  .wraper>>> .swiper-pagination-bullet-active
+    background #fff !important
+  .wraper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 31.25%
+    background-color #eee
+    .swiper-img
+      width 100%
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,16 +2,20 @@
   <div>
     <div class="home">
       <home-header></home-header>
+      <Home-swiper></Home-swiper>
+      <!--<Home-swiper></Home-swiper>-->
     </div>
   </div>
 
 </template>
 <script>
   import HomeHeader from './components/Header.vue'
+  import HomeSwiper from './components/Swiper.vue'
   export default {
     name:'home',
     components:{
-      HomeHeader:HomeHeader
+      HomeHeader,
+      HomeSwiper
     }
   }
 </script>
