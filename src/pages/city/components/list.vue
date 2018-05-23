459ade63d89b1867fb66da2f01ffbca1d6ff704d
lx <690305622@qq.com>
2018-05-23 19:44:19 +0800

    perfomance

--- src/pages/city/components/list.vue
+++ src/pages/city/components/list.vue
@@ -21,10 +21,12 @@
           </div>
         </div>
       </div>
-      <div class="area" v-for="(item,key) of cities" :key="key">
+      <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list" >
-          <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
+          <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
+            {{innerItem.name}}
+          </div>
         </div>
       </div>
     </div>
@@ -37,10 +39,21 @@
     name:'CityList',
     props:{
       hot:[Array,Object],
-      cities:Object
+      cities:Object,
+      letter:String
     },
     mounted (){
       this.scroll=new Bscroll(this.$refs.wrapper)
+    },
+    watch:{
+      letter(){
+        if(this.letter){
+          const element=this.$refs[this.letter][0]
+//          console.log(element)
+          this.scroll.scrollToElement(element)
+        }
+        console.log(this.letter)
+      }
     }
   }
 </script>
